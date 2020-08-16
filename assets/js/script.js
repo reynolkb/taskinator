4b7fa53b71a69379c9f0d6b645393bc41287152b
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-08-15 23:52:25 -0700

    updated file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
-<<<<<<< HEAD
-=======
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
->>>>>>> feature/add-task
 
 var taskFormHandler = function () {
 
